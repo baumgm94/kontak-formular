ba79ef0f4530bd6aaf78732e3bd5b4af414c77b4
Marco Baumgartner <marco.baumgartner94@gmail.com>
2022-07-21 11:04:23 +0200

    submitt button position adjustment

--- my-app/src/components/button.js
+++ my-app/src/components/button.js
@@ -1,10 +1,10 @@
 export { SubmitButton };
 function SubmitButton({ buttonName }) {
   return (
-    <div className="flex justify-start items-start">
+    <div className="flex justify-center items-start">
       <button
         type="submit"
-        className=" text-white w-32 h-12 bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mt-3 mb-8 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        className=" text-white w-32 h-12 bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mt-3 mb-8 mr-64 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       >
         {buttonName}
       </button>
